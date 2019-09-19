62b5cb8abfec37793e6317a32c5f1fe2193af442
diego0096 <dfqz93@hotmail.com>
2019-09-19 20:13:23 +0000

    conditional commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
+{
+char r;
+for (int r = 'a'; r <= 'z'; r++)
+{
+putchar(r);
+putchar("\n");
+}
+return (0);
+}
