092b2b8f81e99dfe50fa4ea2c936fd0c4b48ed90
diego0096 <dfqz93@hotmail.com>
2019-09-19 21:16:17 +0000

    conditional commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int t;
+for (t = 0; t < 10; t++)
+putchar(t + '0');
+putchar('\n');
+return (0);
+}
