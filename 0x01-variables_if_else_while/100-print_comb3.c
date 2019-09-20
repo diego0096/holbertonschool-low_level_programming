4cc4cf41792f0b46529a0f326e5b2eb110c7e93f
diego0096 <dfqz93@hotmail.com>
2019-09-20 01:34:17 +0000

    conditional commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char t, r, u;
+t = 0;
+while (t < 100)
+{
+r = t % 10;
+u = t / 10;
+if (u < r)
+{
+putchar(u + '0');
+putchar(r + '0');
+if (t < 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+t++;
+}
+putchar('\n');
+return (0);
+}
