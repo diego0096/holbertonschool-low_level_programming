c3f861bb1b801f74dcdcded8f6475b192f0be479
diego0096 <dfqz93@hotmail.com>
2019-09-24 13:47:13 +0000

    nested commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * @c: is char type
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
+else
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+In this project let's work on functions and nested loops
