8127c55982f9a7b32a0bcca5e7d8f772e047358f
diego0096 <dfqz93@hotmail.com>
2019-09-24 21:38:38 +0000

    nested commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,26 @@
 #include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print all natural number
+ * @n: int type
+ *
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+	{
+			printf("%d, ", n);
+	}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	}
+	printf("98\n");
+}
