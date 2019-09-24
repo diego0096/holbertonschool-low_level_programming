94ac2a28f3ca85e86884d066ad92ef7327520e04
diego0096 <dfqz93@hotmail.com>
2019-09-24 21:13:24 +0000

    nested commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,28 +6,38 @@
 */
 void times_table(void)
 {
-	int a, b;
+	int a;
+	int b;
 
 	for (a = 0 ; a <= 9 ; a++)
 	{
 		for (b = 0 ; b <= 9 ; b++)
-			if ((a * b) < 9)
+		{
+			if (b == 0)
 			{
-				_putchar(((a * b) / 10) + '0');
-				_putchar(((a * b) % 10) + '0');
-			else if (b != 0)
-			{
-				_putchar(' ');
-				_putcahr((a * b) + '0');
+				_putchar('0');
+				_putchar(',');
+				_putchar(';');
 			}
 			else
-				_putchar((a * b) + '0');
+			{
+				if ((a * b) < 10)
+				_putchar(' ');
+
+			else
+				_putchar(a * b / 10 + '0');
+				_putchar(a * b % 10 + '0');
+
+			if (b == 9)
+			{
+			_putchar('\n');
 			}
-			if (b != 9)
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			_putchar('\n');
+		}
 	}
+}
 }
