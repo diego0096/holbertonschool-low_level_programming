7ada639f15bb4a84af4cd0633a8948024c2d9c85
diego0096 <dfqz93@hotmail.com>
2019-09-24 20:24:17 +0000

    nested commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+# include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ *
+ * @a: int type
+ * @b: int type
+ *
+ * Return: two intergers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
