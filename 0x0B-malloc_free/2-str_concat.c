3f4a95bb69e3a75653ae99bf3d2460c081e85c7d
diego0096 <dfqz93@hotmail.com>
2019-10-16 03:52:25 +0000

    malloc commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenate two string
+ * @s1: char type
+ * @s2: char type
+ *
+ * Return: s
+ */
+char *str_concat(char *s1, char *s2)
+{
+unsigned int a, b, size;
+char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (a = 0 ; s1[a] != '\0' ; a++)
+	for (b = 0 ; s2[b] != '\0' ; b++)
+	size = a + b + 1;
+	s = (char *) malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	for (b = 0; b < size; b++)
+		if (*s1 != '\0')
+			s[b] = *(s1++);
+		else
+			s[b] = *(s2++);
+
+	return (s);
+}
