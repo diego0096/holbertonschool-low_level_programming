780f06357de7b5d43c0f873b47fbcd6982e2abc7
Davis Ramirez <davisantiagoramirez@gmail.com>
2019-10-17 19:49:57 +0000

    more malloc commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int b;
+
+	if (nmemb == 0 || size == 0)
+	
