5cbd237537171f5e8b78e9e54b82f04b4dcec3ae
Davis Ramirez <davisantiagoramirez@gmail.com>
2019-10-17 18:46:47 +0000

    more malloc commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,20 +1,48 @@
-#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
+ * *string_nconcat - check the code for Holberton School students.
  *
- * Return: Always 0.
+ * @s1: destination for concatnation
+ * @s2: source of string
+ * @n: int type for size of byte
+ * Return: A pointer
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int c1, c2;
 	int l1, l2;
 	char *a;
-	int ptr = n
+	int p = n;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	for (l1 = 0 ; s1[l1] != '\0' ; l1++)
+		;
+	for (l2 = 0 ; s2[l2] != '\0' ; l2++)
+		;
+	if (p >= l2)
+	{
+		p = l2;
+		a = malloc(sizeof(char) * (l1 + l2 + 1));
+	}
+	else
+	{
+		a = malloc(sizeof(char) * (l1 + n + 1));
+	}
+	if (a == NULL)
+		return (NULL);
+	for (c1 = 0 ; c1 < l1 ; c1++)
+	{
+		a[c1] = s1[c1];
+	}
+	for (c2 = 0 ; c2 <  p ; c2++)
+	{
+		a[c1++] = s2[c2];
+	}
+	a[c1++] = '\0';
+	return (a);
+}
