a6fa958c74816140d26fbb2ceed909b9fa404c99
Davis Ramirez <davisantiagoramirez@gmail.com>
2019-10-21 20:35:29 +0000

    struc commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MCDOG
+#define MCDOG
+
+/**
+ * struct dog - Defines dogs attributes
+ * @name: A string for the dogs name
+ * @age: An integer for the dogs age
+ * @owner: A string for the owners name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
