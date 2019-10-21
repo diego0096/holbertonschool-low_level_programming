ce354a6fd29eb0a6c299e4a6bbcab17b7490378a
Davis Ramirez <davisantiagoramirez@gmail.com>
2019-10-21 19:18:11 +0000

    preprocessor commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef MACRO_FILE
+#define MACRO_FILE
+
+#define ABS(X) ((X < 0) ? (-X) : (X))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef MACRO_FILE
+#define MACRO_FILE
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
