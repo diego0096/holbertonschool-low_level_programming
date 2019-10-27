04890e7e1f865331ed3999441cd43f5aeeed1c18
diego0096 <dfqz93@hotmail.com>
2019-10-27 16:14:51 +0000

    variadic commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,7 +40,7 @@ void print_all(const char * const format, ...)
 				flag = 1;
 				break;
 		}
-		if (format[i + 1] != '\0' && flag == 0)
+		if (format[a + 1] != '\0' && flag == 0)
 			printf(", ");
 		a++;
 	}
