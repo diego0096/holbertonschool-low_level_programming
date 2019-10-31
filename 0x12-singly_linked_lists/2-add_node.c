0c6b724ee653cbc7742d1fd30d23ca3d38067cca
Davis Ramirez <davisantiagoramirez@gmail.com>
2019-10-31 21:14:21 +0000

    third commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_node - check the code for Holberton School students.
+ *
+ * @head: double pointer of list_t struc type.
+ * @str: pointer of the string input
+ * Return: the address of the new element.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *t;
+	unsigned int c;
+
+	t = malloc(sizeof(list_t));
+		if (t == NULL)
+			return (NULL);
+		for (c = 0 ; str[c] != '\0' ; c++)
+		{
+
+		}
+	t->len = c;
+	t->str = strdup(str);
+	t->next = *head;
+	*head = t;
+	return (*head);
+}
