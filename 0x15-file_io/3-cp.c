7018b7c7f3844c1fa9a449ecf4cd38b9f7533086
diego0096 <dfqz93@hotmail.com>
2019-11-16 17:44:17 +0000

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#define BUFSIZE 1024
+#define BSIZE 1024
 /**
  * main - C program that copied file content from one to another.
  *
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int file1, file2, rd, rt;
-	char buffer[1024];
+	char buffer[BSIZE];
 
 	rt = 1;
 	if (argc != 3)
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	file1 = open(argv[1], O_RDONLY);
 	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	rd = read(file1, buffer, BUFSIZE);
+	rd = read(file1, buffer, BSIZE);
 	if (rd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 		rt = write(file2, buffer, rd);
 		if (rt == -1 || rt != rd)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		rd = read(file1, buffer, BUFSIZE);
+		rd = read(file1, buffer, BSIZE);
 		if (rd == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 				exit(98);
