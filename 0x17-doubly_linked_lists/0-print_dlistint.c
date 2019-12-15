edfd59497f975e165bcb4967eabaa5c9cfe77b84
diego0096 <dfqz93@hotmail.com>
2019-12-15 05:34:16 +0000

    first commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - function to print the elements.
+ *
+ * @h: pointer of const dlistint_t type
+ * Return: return list.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	unsigned int c;
