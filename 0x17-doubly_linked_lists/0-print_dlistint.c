cfba1b2b030dd15623f439faf1678f798e58e6cc
diego0096 <dfqz93@hotmail.com>
2019-12-15 05:32:08 +0000

    first commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,12 +2,12 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    unsigned int c;
+	unsigned int c;
 
-    for (c = 0 ; h != NULL ; c++)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-    }
-    return (c);
+	for (c = 0 ; h != NULL ; c++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (c);
 }
