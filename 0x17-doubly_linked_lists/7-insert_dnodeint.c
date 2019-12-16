42d212e35539172521601eea489b66a2239d94f6
diego0096 <dfqz93@hotmail.com>
2019-12-16 03:49:27 +0000

    eighth commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,50 +1,59 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - Inserts a node.
- *
- * @h: Pointer to a pointer to the first node.
- * @idx: Position for node to be inserted.
- * @n: Number in the node.
- * Return: The address of the new node.
+ * insert_dnodeint_at_index - function to insert node at given index
+ * @h: double pointer type to head of list
+ * @idx: index at which node will be added
+ * @n: int type for data to be added
+ * Return: node if successful, NULL if failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *nd = malloc(sizeof(dlistint_t));
 	dlistint_t *t;
-	dlistint_t *new_node;
-	unsigned int count;
+	unsigned int count = 0;
 
-	if (h == NULL || (*h == NULL && idx != 0))
+	if (nd == NULL)
 		return (NULL);
-	new_node = create_node(n);
-	if (new_node == NULL)
-		return (NULL);
+	nd->n = n;
+	nd->prev = NULL;
+	nd->next = NULL;
+
+	if (*h == NULL)
+	{
+		*h = nd;
+		nd->next = NULL;
+		return (nd);
+	}
 	t = *h;
 	if (idx == 0)
 	{
-		if (*h == NULL)
-			*h = new_node;
-		else
-		{
-			new_node->next = t;
-			t->prev = new_node;
-			*h = new_node;
-		}
-		return (new_node);
+		nd->next = t;
+		t->prev = nd;
+		*h = nd;
+		return (nd);
 	}
-	for (count = 0; t->next != NULL || count + 1 == idx; count++)
+
+	while (count != (idx - 1))
 	{
-		if (count + 1 == idx)
-		{
-			if (t->next != NULL)
-				t->next->prev = new_node;
-			new_node->next = t->next;
-			t->next = new_node;
-			new_node->prev = t;
-			return (new_node);
-		}
 		t = t->next;
+		count++;
+		if (t == NULL)
+		{
+			free(nd);
+			return (NULL);
+		}
 	}
-	free(new_node);
-	return (NULL);
+	nd->next = t->next;
+	nd->prev = t;
+	if (t->next == NULL)
+	{
+		t->next = nd;
+	}
+	else
+	{
+		t->next->prev = nd;
+		t->next = nd;
+	}
+	return (nd);
 }
